74f00f49ce9d31c11affe423992013158fc51e86
eyu-design <eyobm0914@gmail.com>
2025-07-13 18:06:44 +0000

    app.js modify

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage.jsx';
+import WelcomeMessage from './WelcomeMessage
 function App() {
   const [count, setCount] = useState(0)
 
